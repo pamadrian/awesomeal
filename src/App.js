c5985605c21bb660bffd34c063039fdd908bd689
pamadrian <pamvanalfen@icloud.com>
2021-09-28 14:50:20 +0200

    the app finished for today

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
+// src/App.js
 import "./App.css";
+
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Title content="some simple title" />
+        <Title content="Some Simple Title" />
+        <div className="row">
+          {all_pokemon.map((pokemon, index) => (
+            <div key={index} className="col-md-6 col-lg-4">
+              <Pokemon
+                key={index}
+                name={pokemon.name}
+                awesome={pokemon.awesome}
+                terrifying={pokemon.terrifying}
+                abilities={pokemon.abilities}
+              />
+            </div>
+          ))}
+        </div>
       </main>
-      <h1> Welcome to myself and I!</h1>
-      <p>I have an app!</p>
     </div>
   );
 }
---
+++ src/components/Pokemon.js
@@ -0,0 +1,32 @@
+// src/components/Pokemon.js
+
+import "./Pokemon.scss";
+
+export default function Pokemon(props) {
+  return (
+    <div className="Pokemon card shadow-sm mb-4">
+      <div className="card-body pb-0">
+        <h5 className="card-title">{props.name}</h5>
+        <h6 className="card-subtitle mb-3 text-primary">
+          {props.awesome ? "An awesome Pokemon" : "Not awesome"}
+        </h6>
+        <p className="mb-0">
+          Weight: {props.weight} kg
+          <br />
+          Terrifying: {props.terrifying ? "Very" : "nah, lovable"}
+          <br />
+          {props.abilities.length} abilities
+        </p>
+      </div>
+      <ul className="list-group list-group-flush">
+        {props.abilities.map((ability, index) => {
+          return (
+            <li key={index} className="list-group-item">
+              {ability}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
